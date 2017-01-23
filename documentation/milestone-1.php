d096eedb072989d52100f59d4b38256c8dbfd566
Joseph Rohrer <josephrohrer@me.com>
2017-01-23 01:03:13 -0700

    Updated Milestone 1 PHP file

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -23,17 +23,20 @@
 			<p><strong>Name: </strong>Charles Harvey<br>
 				<strong>Age: </strong>37</p>
 
-			<p><strong>Profession: </strong> Charles is a Software Systems Engineer for Sandia National Laboratories.</p>
+			<p><strong>Profession: </strong> Charles is a Software Systems Engineer for Sandia National Laboratories, a national research and development laboratory located in Albuquerque, New Mexico.</p>
 
-			<p><strong>Technology: </strong> .</p>
+			<p><strong>Technology: </strong>Charles used his work's funding to purchase a 15" Macbook Pro, an iPhone 6 Plus. He also has an older Dell desktop that was recently upgraded to Windows 10 to do testing on Internet Explorer and a Samsung Galaxy S5 to test websites on Android.</p>
 
-			<p><strong>Attitudes and Behaviors: </strong> George hates websites that are hard to use. He likes that Engadget is easy to use and that users are able to comment on articles. He also likes that the users who read Engadget are also the people who would potentially buy his robot.</p>
+			<p><strong>Attitudes and Behaviors: </strong> Charles knows that his team needs to hire another member because of the amount of work he is doing on a top secret project. As a friend of his, I reached out to him for potential job opportunities and sent him the link to my website. He's able to pass that along to the hiring manager </p>
 
-			<p><strong>Frustrations and Needs:</strong> George is frustrated with how hard it is to communicate with a mass amount of people about a product. He could email every single person who is interested in Rosie but that would take up all of his time. He needs a medium where he can answer questions from customers and not have to answer the same things over and over again.</p>
+			<p><strong>Frustrations and Needs:</strong> Charles needs to hire someone that
+				local
+
+
+				George is frustrated with how hard it is to communicate with a mass amount of people about a product. He could email every single person who is interested in Rosie but that would take up all of his time. He needs a medium where he can answer questions from customers and not have to answer the same things over and over again.</p>
 
 		<h2>Use Case</h2>
-		<p>George has to stay up on the latest and greatest technology devices for his job, as well as for fun. He reads <a href="http://www.engadget.com">Engadget.com</a> regularly to keep up, as well as to research some of his competition.
-			<br><br>Rosie debuted at the 2017 Consumer Electronics Show and was featured by multiple news and media outlets. Engadget.com did a write-up on Rosie and George would like to see the initial reactions and comments that potential customers have on the site. He can also interact with potential customers and answer questions that they might have.
+		<p>
 
 	</main>
 </html>
