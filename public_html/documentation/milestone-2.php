005aa0cabb2497f38b62782efd9e1c7c38b8c1a5
Joseph Rohrer <josephrohrer@me.com>
2017-02-05 22:37:46 -0700

    Updated Milestone 2 PHP file with Fullscreen Wireframe and added image.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,13 +18,14 @@
 			<strong>Welcome:</strong> The Welcome section will have a professional photograph and a summary paragraph.<br><br>
 			<strong>About Me:</strong> The About Me section will have a list of work experience. It will also feature a list of core skills.<br><br>
 			<strong>Portfolio:</strong> The Portfolio section will contain small thumbnails of marketing, graphic design, and web development work. Each thumbnail on click will then have a larger overlay image to actually showcase the work.<br><br>
-			<strong>Contact:</strong> The Contact section will contain a simple contact form, an email address, and links to my social media profiles: LinkedIn, Twitter, Facebook, and Github.
+			<strong>Contact:</strong> The Contact section will contain a simple contact form, an email address, and links to my social media profiles: LinkedIn, Twitter, Facebook, and Github.<br>
 		</p>
 
 
 		<h2>Wireframes</h2>
 		<h3>Mobile Layout</h3>
 		<h3>Full Screen Layout</h3>
+		<img src="images/wireframe.jpg" alt="Fullscreen Wireframe">
 		<p>
 		</p>
 	</main>
