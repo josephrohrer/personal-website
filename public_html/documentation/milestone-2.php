e46b7970d35073937bbaf6d356b1eb364798d707
Joseph Rohrer <josephrohrer@me.com>
2017-02-05 19:18:34 -0700

    Updated Milestone 2 PHP file with Content Strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -14,10 +14,13 @@
 				Milestone 2a</h1>
 		</header>
 		<h2>Content Strategy</h2>
-		<p><strong>Purpose:</strong> This project is a professional website starting page.<br><br>
-			<strong>Audience:</strong> This site will be targeted towards individuals looking to get more information about me and see some work that i have accomplished. This will include: hiring managers, recruiters, and owners of small businesses.<br><br>
-			<strong>Goal:</strong> The goal of this site will be to showcase some of the work I've done. They will have already seen my resume through an online application or e-mail which will have directed them to this portfolio. They will be able to further connect with me through social media profiles and/or a contact form on the page. This page will also serve as a starting point for a much broader online presence which will include a more extensive portfolio of work as well as a technology blog.
+		<p>This site will be one page and will consist of the following sections:<br><br>
+			<strong>Welcome:</strong> <br><br>
+			<strong>About Me:</strong> <br><br>
+			<strong>Portfolio:</strong> The Portfolio will contain small thumbnails of marketing, graphic design, and web development work. Each thumbnail on click will then have a larger overlay image to actually showcase the work.<br><br>
+			<strong>Contact:</strong> This section will contain a simple contact form, an email address, and links to my social media profiles: LinkedIn, Twitter, Facebook, and Github.
 		</p>
+
 
 		<h2>Wireframes</h2><br>
 		<h3>Mobile Layout</h3>
