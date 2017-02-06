4917e0a4273909dbdf3c2ca657aa54649d12079b
Joseph Rohrer <josephrohrer@me.com>
2017-02-05 18:56:54 -0700

    Created Milestone 2 PHP file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- load custom css here -->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+		<title>Personal Website Project - Milestone 2a</title>
+	</head>
+	<main>
+		<header>
+			<h1>Personal Website Project<br>
+				Milestone 2a</h1>
+		</header>
+		<h2>Content Strategy</h2>
+		<p><strong>Purpose:</strong> This project is a professional website starting page.<br><br>
+			<strong>Audience:</strong> This site will be targeted towards individuals looking to get more information about me and see some work that i have accomplished. This will include: hiring managers, recruiters, and owners of small businesses.<br><br>
+			<strong>Goal:</strong> The goal of this site will be to showcase some of the work I've done. They will have already seen my resume through an online application or e-mail which will have directed them to this portfolio. They will be able to further connect with me through social media profiles and/or a contact form on the page. This page will also serve as a starting point for a much broader online presence which will include a more extensive portfolio of work as well as a technology blog.
+		</p>
+
+		<h2>Wireframes</h2><br>
+		<h3>Mobile Layout</h3>
+		<p>
+		</p>
+		<h3>Full Screen Layout</h3>
+		<p>
+		</p>
+	</main>
+</html>
